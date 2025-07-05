da79064b16526fab5907cdaa1f6dbce285ab81e8
meoba <meoba@192.168.1.8>
2025-07-05 21:55:23 +0700

    add connectDB

---
+++ src/ulti/ConnectDB.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ulti;
+
+
+import java.sql.*;
+
+/**
+ *
+ * @author luand
+ */
+public class ConnectDB {
+    public static Connection con = null;
+    public static Connection getConnect() {
+        String strDbUrl = "jdbc:sqlserver://localhost:1433; databaseName=quanLyBan;user=sa;password=1234;"
+                + "encrypt=true;trustServerCertificate=true";
+        try {
+            con = DriverManager.getConnection(strDbUrl);
+            System.out.println("Ket Noi Thanh Cong");
+        } catch (SQLException e) {
+            System.out.println("Connect Loi: " + e);
+        }
+        return con;
+    }
+    public static void main(String[] args) {
+        ConnectDB.getConnect();
+    }
+}
